c4646c5044cd2508aaa22a1add10dd5e781cb0a0
despencer <despencer@mail.ru>
2026-04-04 13:11:26 +0300

    Testing mode was added for the interface.

--- interface.py
+++ interface.py
@@ -6,6 +6,35 @@ def select(lookup, item):
         raise Exception(f'Unknown option {itemtype}')
     return lookup[itemtype], item[itemtype]
 
+class Test:
+    def __init__(self, body):
+        self.body = body
+
+    def do(self, prompt):
+        logging.info('Entering test mode')
+        for yfunc in prompt:
+            func, value = select( {'resolve': self.resolve}, yfunc)
+            func(value)
+        logging.info('Leaving test mode')
+
+    def resolve(self, ytest):
+        logging.info(f'About to test resolve {ytest}')
+        results = self.body.resolve_strings( ytest['args'], list(ytest['targets'].keys()) )
+        if len(results) != 1:
+            print(f'Bad number of results {len(results)}')
+        else:
+            results = results[0]
+            for k,v in ytest['targets'].items():
+                ak = self.body.getatom(k)
+                if ak not in results:
+                    print(f"key '{k}' is not found among results '{results}' in test {ytest}")
+                else:
+                    if results[ak] != self.body.getatom(v):
+                        print(f"Different result for '{k}': {results[ak]} vs '{v}' in test {ytest}")
+            if len(results) != len(ytest['targets']):
+                print(f"Different number of results {len(results)} vs {len(ytest['targets'])} in test {ytest}")
+        logging.info(f'Test {ytest} finished')
+
 class Maintenance:
     def __init__(self, body):
         self.body = body
@@ -28,6 +57,6 @@ class Interface:
 
     def do(self, prompt):
         for ymode in prompt:
-            cls, value = select( {'maintenance':Maintenance}, ymode)
+            cls, value = select( {'maintenance':Maintenance, 'test':Test}, ymode)
             mode = cls(self.body)
             mode.do(value)
